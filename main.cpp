31ed59a8327392cdf39e2c2b3cb78c2b91e75c79
arisharma43 <70657612+arisharma43@users.noreply.github.com>
2021-07-03 18:07:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     char board[3][3] = {{'#','#', '#'},{'#','#', '#'},{'#','#', '#'}};
     char player = 'X';
+    char winner = '#';
     for(int p=0;p<9;p++){
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -22,10 +23,53 @@ int main()
             board[row-1][col-1]=player;
         }else{
             system("cls");
-            cout << "Invalid move" << end1;
+            cout << "Invalid move" << endl;
             continue;
         }
+        bool ended=false;
+        for(int i=0;i<3;i++){
+            if(board[i][0]==player && board[i][1]==player&&board[i][2]==player){
+                ended=true;
+                winner=player;
+                break;
+            }
+        }
+        if(ended){
+            break;
+        }
+        for(int i=0;i<3;i++){
+            if(board[0][i]==player && board[1][i]==player&&board[2][i]==player){
+                ended=true;
+                winner=player;
+                break;
+            }
+        }
+        if(ended){
+            break;
+        }
+        if(board[0][0]==player && board[1][1]==player&&board[2][2]==player){
+            winner=player;
+            break;
+        }
+        if(board[0][0]==player && board[1][1]==player&&board[2][2]==player){
+            winner=player;
+            break;
+        }
+        if(board[0][2]==player && board[1][1]==player&&board[2][0]==player){
+            winner=player;
+            break;
+        }
+        if(player=='X')
+            player='O';
+        else
+            player='X';
+        system("cls");
     }
-    cout << "Hello world!" << endl;
+    if(winner=='X')
+        cout<<"Player X has won"<<endl;
+    else if(winner =='O')
+        cout<<"Player O has won"<<endl;
+    else
+        cout<<"It's a draw"<<endl;
     return 0;
 }
